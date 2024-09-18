d9779dc97bca0c3bf02ac456e12365c3ca0fc17f
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-18 11:03:30 +0300

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -6,7 +6,7 @@ zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest,
     let z = '*🌍𝑩𝒐𝒕 𝒊𝒔 𝒐𝒏𝒍𝒊𝒏𝒆🌍* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
     let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
+    var mp4 = 'https://i.imgur.com/L4U2K76.jpeg';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
