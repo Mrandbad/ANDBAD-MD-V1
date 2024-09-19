4fa7e18903c33d6598e69b9b5523e662f97739e5
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-19 22:17:23 +0300

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'AM ALIVE STUPID.... GO GITHUB AND SEARCH ANDBAD MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$';
+    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH ANDBAD MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
     let d = '                                                                           I LOVE MY OWNER';
     let varmess = z + d;
     var img = 'https://i.imgur.com/L4U2K76.jpeg';
