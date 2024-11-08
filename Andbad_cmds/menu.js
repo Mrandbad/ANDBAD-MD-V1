155b280e2234d134ad9096089b831b816d22ee0d
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-11-08 17:21:24 +0300

    Add files via upload

--- Andbad_cmds/menu.js
+++ Andbad_cmds/menu.js
@@ -1,62 +1,96 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
 
-const util = require('util'); const fs = require('fs-extra'); const { zokou } = require(__dirname + "/../framework/zokou"); const { format } = require(__dirname + "/../framework/mesfonctions"); const os = require("os"); const moment = require("moment-timezone"); const s = require(__dirname + "/../set"); const more = String.fromCharCode(8206) const readmore = more.repeat(4001)
-
-zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => { let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions; let { cm } = require(__dirname + "/../framework//zokou"); var coms = {}; var mode = "public";
-
-
-Verify
-Edit
-Copy code
-if ((s.MODE).toLocaleLowerCase() != "yes") {
-    mode = "private";
-}
+zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
 
 
+    
 
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
 
-cm.map(async (com, index) => {
-    if (!coms[com.categorie])
-        coms[com.categorie] = [];
-    coms[com.categorie].push(com.nomCom);
-});
+    moment.tz.setDefault('Etc/GMT');
 
-moment.tz.setDefault('Etc/GMT');
-// Créer une date et une heure en GMT const temps = moment().format('HH:mm:ss'); const date = moment().format('DD/MM/YYYY');
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
 
-let infoMsg = `
-
-ANDBAD MD AVAILABLE MENUS* ╭───────────────── │❒⁠⁠⁠⁠╭───────────── │❒⁠⁠⁠⁠│▸ MENU │❒⁠⁠⁠⁠│▸ MENU2 │❒⁠⁠⁠⁠│▸ BUGMENU │❒⁠⁠⁠⁠╰────────────── │❒⁠⁠⁠⁠│▸ PLUGINS : ${cm.length} │❒⁠⁠⁠⁠│▸ RAM : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())} │❒⁠⁠⁠⁠│▸ SAVER : ${os.platform()} │❒⁠⁠⁠⁠│▸ THEME : ANDBADTHEMES │❒⁠⁠⁠⁠╰────────────── ╰──────────────────\n`;
-
+  let infoMsg =  `
+> ANDBAD MD AVAILABLE MENUS* 
+╭─────────────────
+│❒⁠⁠⁠⁠╭─────────────
+│❒⁠⁠⁠⁠│▸ *MENU* 
+│❒⁠⁠⁠⁠│▸ *MENU2* 
+│❒⁠⁠⁠⁠│▸ *BUGMENU*
+│❒⁠⁠⁠⁠╰──────────────
+│❒⁠⁠⁠⁠│▸ *PLUGINS* : ${cm.length} 
+│❒⁠⁠⁠⁠│▸ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│❒⁠⁠⁠⁠│▸ *SAVER* : ${os.platform()}
+│❒⁠⁠⁠⁠│▸ *THEME* : *ANDBADTHEMES*
+│❒⁠⁠⁠⁠╰──────────────
+╰──────────────────\n`;
+    
 let menuMsg = `
 
-COMMANDS${readmore} `;
-
-
-Verify
-Edit
-Copy code
-for (const cat in coms) {
-    menuMsg += ` ╭────────❒⁠⁠⁠⁠ *${cat}* ✣`;
-    for (const cmd of coms[cat]) {
-        menuMsg += `
-│❒⁠⁠⁠⁠│▸ ${cmd}; } menuMsg +=  ╰────────────···▸▸ \n` }
-
-
-Verify
-Edit
-Copy code
-menuMsg += `> MADE EASY BY MR ANDBAD 
+ *COMMANDS*${readmore}
 `;
 
-var lien = mybotpic();
+    for (const cat in coms) {
+        menuMsg += ` ╭────────❒⁠⁠⁠⁠ *${cat}* ✣`;
+        for (const cmd of coms[cat]) {
+            menuMsg += `
+│❒⁠⁠⁠⁠│▸ ${cmd}`;
+        }
+        menuMsg += `
+╰────────────···▸▸ \n`
+    }
 
-if (lien.match(/.(mp4|gif)$/i)) { try { zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis msela-chui-v2, déveloper mselachui Tech" , gifPlayback : true }, { quoted: ms }); } catch (e) { console.log("🥵🥵 Menu erreur " + e); repondre("🥵🥵 Menu erreur " + e); } } // Vérification pour .jpeg ou .png else if (lien.match(/.(jpeg|png|jpg)$/i)) { try { zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis msela-chui-v2, déveloper mselachui Tech" }, { quoted: ms }); } catch (e) { console.log("🥵🥵 Menu erreur " + e); repondre("🥵🥵 Menu erreur " + e); } } else {
+    menuMsg += `> MADE EASY BY MR ANDBAD 
+`;
 
+   var lien = mybotpic();
 
-Verify
-Edit
-Copy code
-repondre(infoMsg + menuMsg);
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *msela-chui-v2*, déveloper mselachui Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *msela-chui-v2*, déveloper mselachui Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
 }
 
 });
