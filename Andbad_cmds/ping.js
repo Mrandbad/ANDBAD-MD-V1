47747d49493a5e846f3897cdea9cda5b8b5d9e87
ANDBAD-MD-V1 <mallyaandrea361@gmail.com>
2024-11-16 13:51:13 +0300

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH ANDBAD MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
-    let d = '                                                                           I LOVE MY OWNER';
+    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH 𝚫𝚴𝐃𝚩𝚫𝐃 MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
+    let d = '                                                                           I LOVE MY OWNER 𝚳𝚪 𝚫𝚴𝐃𝚩𝚫𝐃';
     let varmess = z + d;
     var img = 'https://i.imgur.com/L4U2K76.jpeg';
     await zk.sendMessage(dest, { video: { url: img }, caption: varmess });
