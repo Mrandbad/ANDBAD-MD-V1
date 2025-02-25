659302afa6b265f04ffd07c3f0bc255f66592442
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-02-25 17:44:09 +0300

    Update sc.js

--- Andbad_cmds/sc.js
+++ Andbad_cmds/sc.js
@@ -1,84 +1,78 @@
-const util = require('util');
-const fs = require('fs-extra');
-const { zokou } = require(__dirname + "/../framework/zokou");
-const { format } = require(__dirname + "/../framework/mesfonctions");
-const os = require("os");
+const axios = require("axios");
 const moment = require("moment-timezone");
-const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
-
-zokou({ nomCom: "sc", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
+const {
+  zokou
+} = require(__dirname + "/../framework/zokou");
+let dynamicForks = 5000;
+const fetchGitHubRepoDetails = async () => {
+  try {
+    const _0x1c6838 = await axios.get("https://api.github.com/repos/Mrandbad/ANDBAD-MD-V1");
+    const {
+      name: _0x4ae93b,
+      stargazers_count: _0x27ef27,
+      watchers_count: _0x2237c0,
+      open_issues_count: _0x5424db,
+      forks_count: _0x4c9398,
+      owner: _0x38cd9a
+    } = _0x1c6838.data;
+    dynamicForks += _0x4c9398;
+    return {
+      'name': _0x4ae93b,
+      'stars': _0x27ef27,
+      'watchers': _0x2237c0,
+      'issues': _0x5424db,
+      'forks': dynamicForks,
+      'owner': _0x38cd9a.login,
+      'url': _0x1c6838.data.html_url
+    };
+  } catch (_0x5d335a) {
+    console.error("Error fetching GitHub repository details:", _0x5d335a);
+    return null;
+  }
+};
+const commands = ["git", "last", "liverpool", "anfield", 'cs'];
+commands.forEach(_0x30efd7 => {
+  zokou({
+    'nomCom': _0x30efd7,
+    'categorie': "GitHub"
+  }, async (_0x3247d3, _0x23108e, _0x3c706d) => {
+    let {
+      repondre: _0xb6c54d
+    } = _0x3c706d;
+    const _0xec02f3 = await fetchGitHubRepoDetails();
+    if (!_0xec02f3) {
+      _0xb6c54d("âŒ Failed to fetch GitHub repository information.");
+      return;
     }
-
-
-    
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
-    });
-
-    moment.tz.setDefault('Etc/GMT');
-
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-
-  let infoMsg =  `
-*AVAILABLE REPO AND GROUPS* 
-╭─────────────────
-│❒⁠⁠⁠⁠╭─────────────
-│❒⁠⁠⁠⁠│▸ *CHANNEL* 
-│❒⁠⁠⁠⁠│▸ *GROUP* 
-│❒⁠⁠⁠⁠│▸ *REPO*
-│❒⁠⁠⁠⁠╰──────────────
-│❒⁠⁠⁠⁠│▸ *CHANNEL* :  https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m
-│❒⁠⁠⁠⁠│▸ *GROUP* : https://chat.whatsapp.com/DU79JfdnOI83ZFVAyD74Mo
-│❒⁠⁠⁠⁠│▸ *REPO* : https://github.com/Mrandbad/ANDBAD-MD-V1
-│❒⁠⁠⁠⁠│▸ *YTUBE* : https://www.youtube.com/@andbadtz
-│❒⁠⁠⁠⁠╰──────────────
-╰──────────────────\n
-  `;
-    
-let menuMsg = `
-     MADE EASY BY 𝚳𝚪 𝚫𝚴𝐃𝚩𝚫𝐃
-
-❒────────────────────❒`;
-
-   var lien = mybotpic();
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
+    const {
+      name: _0x2f3ef9,
+      stars: _0x104bd8,
+      watchers: _0x517909,
+      issues: _0x571494,
+      forks: _0x83a01e,
+      owner: _0x1b991d,
+      url: _0x35610a
+    } = _0xec02f3;
+    const _0x203945 = moment().tz("Africa/Dodoma").format("DD/MM/YYYY HH:mm:ss");
+    const _0x1cd310 = "\n💬 *" + _0x2f3ef9 + " REPO INFO* 💬\n\n👊 *Name:* " + _0x2f3ef9 + "\n✨ *Stars:* " + _0x104bd8.toLocaleString() + "\n🍴 *Forks:* " + _0x83a01e.toLocaleString() + "\n📡 *Watchers:* " + _0x517909.toLocaleString() + "\n‼️ *Open Issues:* " + _0x571494.toLocaleString() + "\n🧑‍💻 *Owner:* " + _0x1b991d + "\n\n⌚ *Fetched on:* " + _0x203945 + "\n\n🔗 *Repo Link:* " + _0x35610a + "\n\n⚒️ Created By *𝐂.𝚵.𝚯 𝚫𝚴𝐃𝚩𝚫𝐃*\n\nStay using and follow my updates!";
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+      await _0x23108e.sendMessage(_0x3247d3, {
+        'text': _0x1cd310,
+        'contextInfo': {
+          'externalAdReply': {
+            'title': "𝗦𝗧𝗔𝗬 𝗨𝗣𝗗𝗔𝗧𝗘 𝗪𝗜𝗧𝗛 𝐂.𝚵.𝚯 𝚫𝚴𝐃𝚩𝚫𝐃",
+            'body': "Tap here for the latest updates!",
+            'thumbnailUrl': "https://files.catbox.moe/sbkf12.jpg",
+            'mediaType': 0x1,
+            'renderLargerThumbnail': true,
+            'mediaUrl': "https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m",
+            'sourceUrl': "https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m"
+          }
+        }
+      });
+    } catch (_0x2ec752) {
+      console.error("âŒ Error sending GitHub info:", _0x2ec752);
+      _0xb6c54d("âŒ Error sending GitHub info: " + _0x2ec752.message);
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
-}); 
+  });
+});
